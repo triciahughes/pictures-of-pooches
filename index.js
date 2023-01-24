2a8df22d0433f6a4db33dc57e534ce9a37ca7e94
triciahughes <hughes.a.tricia@gmail.com>
2023-01-24 09:59:28 -0800

    comments button event and posting

--- index.js
+++ index.js
@@ -7,27 +7,34 @@ fetch(BASE_URL)
     dogImage.src = img.message;
   });
 
+const likeButton = document.getElementById("like-button");
 
-  const likeButton = document.getElementById('like-button');
+const likeParagraph = document.createElement("p");
+likeParagraph.id = "like-count";
+let count = 0;
 
-  const likeParagraph = document.createElement('p')
-  likeParagraph.id = 'like-count'
-  let count = 0
-  
-  const div = document.getElementById('button-container')
-  div.append(likeParagraph)
-  
-  
-  likeButton.addEventListener('click', (event) => {
-    console.log('yay')  
-    const likes = ++count
-      if (count === 0) {
-          likeParagraph.textContent = '0 Likes'
-      } else if (count === 1) {
-          likeParagraph.textContent = `1 Like`
-      }
-      else {
-          likeParagraph.textContent = `${likes} Likes`
-      }
-      
-  })
+const div = document.getElementById("button-container");
+div.append(likeParagraph);
+
+likeButton.addEventListener("click", (event) => {
+  console.log("yay");
+  const likes = ++count;
+  if (count === 0) {
+    likeParagraph.textContent = "0 Likes";
+  } else if (count === 1) {
+    likeParagraph.textContent = `1 Like`;
+  } else {
+    likeParagraph.textContent = `${likes} Likes`;
+  }
+});
+
+const form = document.getElementById("comments");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const comments = document.getElementById("comments-container");
+  const newComment = event.target["new-comment-description"].value;
+  const newP = document.createElement("p");
+  newP.textContent = newComment;
+  comments.appendChild(newP);
+  form.reset();
+});
