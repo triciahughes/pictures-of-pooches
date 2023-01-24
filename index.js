dbff06371a084b8db5e1579f0655860f0b902d79
Brett de Bear <bkdebear@gmail.com>
2023-01-24 08:21:13 -0800

    like button and div

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ fetch(BASE_URL)
   .then((img) => {
     dogImage.src = img.message;
   });
+
+
+  const likeButton = document.getElementById('like-button');
+
+  const likeParagraph = document.createElement('p')
+  likeParagraph.id = 'like-count'
+  let count = 0
+  
+  const div = document.getElementById('button-container')
+  div.append(likeParagraph)
+  
+  
+  likeButton.addEventListener('click', (event) => {
+    console.log('yay')  
+    const likes = ++count
+      if (count === 0) {
+          likeParagraph.textContent = '0 Likes'
+      } else if (count === 1) {
+          likeParagraph.textContent = `1 Like`
+      }
+      else {
+          likeParagraph.textContent = `${likes} Likes`
+      }
+      
+  })
